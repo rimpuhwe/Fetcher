ef6feaadb08ac1c0d85e9f22675f1d5a97055ac4
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-05-08 14:16:09 +0200

    add the thread in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
+        URLFetcherThread threadFirst = new URLFetcherThread("https://www.lipsum.com/");
+        URLFetcherThread threadSecond = new URLFetcherThread("https://www.igihe.com/");
+        Thread thread1 = new Thread(threadFirst , "URLFetcherThread1");
+        Thread thread2 = new Thread(threadSecond , "URLFetcherThread2");
+        thread1.start();
+        thread2.start();
+
 
     }
 }
