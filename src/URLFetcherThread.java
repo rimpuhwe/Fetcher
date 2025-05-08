7dfbdc988a5d56ec5110c2b2c3a488df82b8676c
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-05-08 11:53:38 +0200

    add the buffered read with inputStream class to read from the url

--- src/URLFetcherThread.java
+++ src/URLFetcherThread.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,8 +18,16 @@ public class URLFetcherThread implements Runnable {
     public void run() {
         try {
             URL url = new URL(urlLink);
+            BufferedReader reading = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reading.readLine()) != null) {
+                System.out.println("Thread [" + Thread.currentThread().getName() + "]: Line from "+ urlLink +" : " + line);
+            }
+
         } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            System.out.println("Thread [" + Thread.currentThread().getName() + "]: invalid URL - " + urlLink);
+        } catch (IOException e) {
+            System.out.println("Error in reading from URL - " + urlLink);
         }
 
 
